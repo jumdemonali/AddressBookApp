f48ac08736f539fdb7897a289f821ae673d571f8
jumdemonali <jumdemonali@gmail.com>
2021-10-22 08:40:39 +0530

    Updated : Handling cancel and reset event

--- js/addressData.js
+++ js/addressData.js
@@ -1,50 +1,72 @@
 class AddressBookData {
 
-    get id() {return this._id; }
-    set id(id) {
-        this._id = id;
-    }
+
+        get id() {
+            return this._id;
+        }
+        set id(id) {
+            this._id = id;
+        }
     
-    get name() { return this._name; }
-    set name(name) {
-        let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
-        if (nameRegex.test(name))
-            this._name = name;
-        else throw 'Name is Incorrect!';
+        get name() {
+            return this._name;
+        }
+        set name(name) {
+            let nameRegex = RegExp("^[A-Z]{1}[A-z ]{2,}$");
+            if (nameRegex.test(name)) {
+                this._name = name;
+            } else {
+                throw "Invalid Name";
+            }
+        }
+    
+        get phoneNumber() {
+            return this._phoneNumber;
+        }
+        set phoneNumber(phoneNumber) {
+            let phoneNumberRegex = RegExp("^[0-9]{2}[ ][0-9]{10}$");
+            if (phoneNumberRegex.test(phoneNumber)) {
+                this._phoneNumber = phoneNumber;
+            } else {
+                throw "Invalid phone number";
+            }
+        }
+    
+        get address() {
+            return this._address;
+        }
+        set address(address) {
+            this._address = address;
+        }
+    
+        get city() {
+            return this._city;
+        }
+        set city(city) {
+            this._city = city;
+        }
+    
+        get state() {
+            return this._state;
+        }
+        set state(state) {
+            this._state = state;
+        }
+    
+        get zip() {
+            return this._zip;
+        }
+        set zip(zip) {
+            let zipRegex = RegExp("^[0-9]{3}[ ]?[0-9]{3}$");
+            if (zipRegex.test(zip)) {
+                this._zip = zip;
+            } else {
+                throw "Zip is incorrect";
+            }
+        }
+    
+        toString() {
+            return `id ${this.id} Name ${this.name} phoneNumber ${this.phoneNumber} Address ${this.address} City ${this.city} State ${this.state} Zip  ${this.zip}`
+        }
+    
     }
-
-    get phoneNumber() { return this._phoneNumber; }
-    set phoneNumber(phoneNumber) {
-        let phoneNumberRegex = RegExp('[91]{2}[0-9][0-9]{9}');
-        if (phoneNumberRegex.test(phoneNumber))
-            this._phoneNumber = phoneNumber;
-        else throw 'Phone Number is Incorrect!';
-    }
-
-    get address() { return this._address; }
-    set address(address) {
-        this._address = address;
-    }
-
-    get city() { return this._city; }
-    set city(city) {
-        this._city = city;
-    }
-
-    get state() { return this._state; }
-    set state(state) {
-        this._state = state;
-    }
-
-    get zipCode() { return this._zipCode; }
-    set zipCode(zipCode) {
-        this._zipCode = zipCode;
-    }
-
-    toString() {
-        return "Full Name = " + this.name + ", Phone Number = "
-                    + this.phoneNumber + ", address = " + this.address + 
-                    ", city = " + this.city + ", state = " + this.state +
-                    ", zip code = " + this.zipCode;
-    }
-}
